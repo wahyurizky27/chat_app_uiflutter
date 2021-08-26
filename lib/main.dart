3364a3776ad3389a2e054769553c31a25cd37e20
Wahyu Rizky Ramadhan <64141752+wahyurizky27@users.noreply.github.com>
2021-08-26 21:37:29 +0700

    change title again

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat App',
+      title: 'Chat App Date',
       theme: ThemeData(
         // This is the theme of your application.
         //
